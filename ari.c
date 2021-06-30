b26338f25a0b689c29c76d9cd7c67964467dc6c9
Bhagathvp <84322745+Bhagathvp@users.noreply.github.com>
2021-06-30 16:16:17 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -3,6 +3,8 @@
 #include<ctype.h>
 #include<string.h>
 
+
+
 string ari(string s){
 
 printf("THE ENTERED STRING IS: \n%s\n",s);
@@ -24,6 +26,40 @@ printf("THE ENTERED STRING IS: \n%s\n",s);
 	
 printf("\nthe given string has %li characters, %li words and %li sentences.\n",characters,words,sentences);
 	float score=(4.71*characters/words)+(0.5*words/sentences)-21.43;
-	printf("THE ARI SCORE IS:%i\n",(int)score+1);
-}
+	int ari=(int)score+1;
 	
+	printf("THE ARI SCORE IS:%i.\n",ari);
+	switch(ari){
+	 case 1: printf("The text is suitable for kindergarden.\n");
+	 	 break;
+	 case 2: printf("The text is suitable for  First/Second Grade.\n");
+	 	 break;
+	 case 3: printf("The text is suitable for Third Grade.\n");
+	 	 break;
+	 case 4: printf("The text is suitable for  Fourth Grade.\n");
+	 	 break;
+	 case 5: printf("The text is suitable for Fifth Grade.\n");
+	 	 break;
+	 case 6: printf("The text is suitable for Sixth Grade.\n");
+	 	 break;
+	 case 7: printf("The text is suitable for Seventh Grade.\n");
+	 	 break;
+	 case 8: printf("The text is suitable for Eighth Grade.\n");
+	 	 break;
+	 case 9: printf("The text is suitable for Ninth Grade.\n");
+	 	 break;
+	 case 10: printf("The text is suitable for  Tenth Grade.\n");
+	 	  break;
+	 case 11: printf("The text is suitable for Eleventh Grade.\n");
+	 	  break;
+	 case 12: printf("The text is suitable for Twelfth grade.\n");
+	 	  break;
+	 case 13: printf("The text is suitable for College student.\n");
+	 	  break;
+	 case 14: printf("The text is suitable for Professor.\n");
+	 	  break;
+	 default: printf("The text is not suitable for reading.\n");
+	 }
+	
+	
+}
