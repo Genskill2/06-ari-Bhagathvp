455ade5e9ee110f7c70a0342c1e8b6dbf2040e66
Bhagathvp <84322745+Bhagathvp@users.noreply.github.com>
2021-06-30 17:25:23 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,68 +1,35 @@
 #include<stdio.h>
+#include<string.h>
 #include<cs50.h>
 #include<ctype.h>
-#include<string.h>
-
-
+#include<math.h>
 
 string ari(string s){
-
-printf("THE ENTERED STRING IS: \n%s\n",s);
-	long int characters=0,words=1,sentences=0;
-	for(int i=0;i<strlen(s);i++){
-	   if (isalnum(s[i]))        
-	   characters++;
-	  }
+	int len,num_word=0,num_char=0,num_sent=0;
+	string grade;
+	len = strlen(s);
+	//Find the number of characters, words and sentences.
+	for(int i=0;i<len;i++){
+	  if(isalnum(s[i]))
+	    num_char++;
+	  if(s[i]==' ')
+	    num_word++;
+	  if(s[i]=='!'||s[i]=='?'||s[i]=='.')
+	    num_sent++;
+	    }
+	//ARI value formula
+	float ari_value = 4.71*num_char/num_word+0.5*num_word/num_sent-21.43;
+	//Round Resultant score to next highest integer
+	int r=round(ari_value);
+	if(r<ari_value)
+	  r++;
+	//Return the matching string
+	string grade_level[] = {"KinderKindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"} ;
 	
-	for(int i=0;i<strlen(s);i++){
-	   if(s[i]==' ')                    
-	   words+=1;	
-	  }
-	   
-	for(int i=0;i<strlen(s);i++){
-	   if(s[i]=='.'||s[i]=='!'||s[i]=='?')  
-	   sentences+=1;	
-	  }
-	
-printf("\nthe given string has %li characters, %li words and %li sentences.\n",characters,words,sentences);
-
-	float score=(4.71*characters/words)+(0.5*words/sentences)-21.43;
-	int ari=(int)score+1;
-	string std;
-	
-	switch(ari){
-	 case 1: std="kindergraden";
-	 	 break;
-	 case 2: std="First/Second Grade";
-	 	 break;
-	 case 3: std="Third Grade";
-	 	 break;
-	 case 4: std="Fourth Grade";
-	 	 break;
-	 case 5: std="Fifth Grade";
-	 	 break;
-	 case 6: std="Sixth Grade";
-	 	 break;
-	 case 7: std="Seventh Grade";
-	 	 break;
-	 case 8: std="Eighth Grade";
-	 	 break;
-	 case 9: std="Ninth Grade";
-	 	 break;
-	 case 10: std="Tenth Grade";
-	 	  break;
-	 case 11: std="Eleventh Grade";
-	 	  break;
-	 case 12: std="Twelfth Grade";
-	 	  break;
-	 case 13: std="College student";
-	 	  break;
-	 case 14: std="Professor";
-	 	  break;
-	 default: printf("The text is not suitable for reading.\n");
-	 }
-	 
-	 printf("THE ARI SCORE IS:%i.The text is suitable for %s.\n",ari,std);
-	 return std;
-	
-}
+	for(int i =1;i<=14;i++){
+	 if(r==i)
+	   grade = grade_level[i-1];
+		
+	}
+	return grade;
+	}
