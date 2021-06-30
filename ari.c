938c415b3f38d5ba9e0655a4d98ac43fd8f05d41
Bhagathvp <84322745+Bhagathvp@users.noreply.github.com>
2021-06-30 16:30:00 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -25,49 +25,44 @@ printf("THE ENTERED STRING IS: \n%s\n",s);
 	  }
 	
 printf("\nthe given string has %li characters, %li words and %li sentences.\n",characters,words,sentences);
+
 	float score=(4.71*characters/words)+(0.5*words/sentences)-21.43;
 	int ari=(int)score+1;
+	string std;
 	
-	printf("THE ARI SCORE IS:%i.\n",ari);
 	switch(ari){
-	 case 1: printf("The text is suitable for kindergarden.\n");
+	 case 1: std="kindergraden";
 	 	 break;
-	 case 2: printf("The text is suitable for  First/Second Grade.\n");
+	 case 2: std="First/Second Grade";
 	 	 break;
-	 case 3: printf("The text is suitable for Third Grade.\n");
+	 case 3: std="Third Grade";
 	 	 break;
-	 case 4: printf("The text is suitable for  Fourth Grade.\n");
+	 case 4: std="Fourth Grade";
 	 	 break;
-	 case 5: printf("The text is suitable for Fifth Grade.\n");
+	 case 5: std="Fifth Grade";
 	 	 break;
-	 case 6: printf("The text is suitable for Sixth Grade.\n");
+	 case 6: std="Sixth Grade";
 	 	 break;
-	 case 7: printf("The text is suitable for Seventh Grade.\n");
+	 case 7: std="Seventh Grade";
 	 	 break;
-	 case 8: printf("The text is suitable for Eighth Grade.\n");
+	 case 8: std="Eighth Grade";
 	 	 break;
-	 case 9: printf("The text is suitable for Ninth Grade.\n");
+	 case 9: std="Ninth Grade";
 	 	 break;
-	 case 10: printf("The text is suitable for  Tenth Grade.\n");
+	 case 10: std="Tenth Grade";
 	 	  break;
-	 case 11: printf("The text is suitable for Eleventh Grade.\n");
+	 case 11: std="Eleventh Grade";
 	 	  break;
-	 case 12: printf("The text is suitable for Twelfth grade.\n");
+	 case 12: std="Twelfth Grade";
 	 	  break;
-	 case 13: printf("The text is suitable for College student.\n");
+	 case 13: std="College student";
 	 	  break;
-	 case 14: printf("The text is suitable for Professor.\n");
+	 case 14: std="Professor";
 	 	  break;
 	 default: printf("The text is not suitable for reading.\n");
 	 }
+	 
+	 printf("THE ARI SCORE IS:%i.The text is suitable for %s.\n",ari,std);
 	
 	
-}
-	   
-	
-
-int main(void){
-	string a="Pet ownership is an essential environmental exposure that might influence the health of the owner. This study’s primary objectives were to explore the effects of cat ownership on the gut microbial diversity and composition of owners. Raw data from the American Gut Project were obtained from the SRA database. A total of 214 Caucasian individuals (111 female) with cats and 214 individuals (111 female) without cats were used in the following analysis. OTU number showed significant alteration in the Cat group and Female_cat group, compared with that of the no cat (NC) group and Female_ NC group, respectively. Compared with the NC group, the microbial phylum Proteobacteria was significantly decreased in the Cat group.";
-	ari(a);
-
 }
